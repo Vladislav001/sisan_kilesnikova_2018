b2d96692ecb3dbc697810647612466eb7387a5e4
Vladislav001 <VladGurjev@mail.ru>
2018-05-08 00:22:19 +0300

    Подчистил

--- app.js
+++ app.js
@@ -8,28 +8,27 @@ var flash = require('connect-flash');
 
 var dbConfig = require('./db');
 var mongoose = require('mongoose');
-// Connect to DB
+
 mongoose.connect(dbConfig.url);
 
 
 var app = express();
 
-// use ejs-locals for all ejs templates:
+
 app.engine('ejs', require('ejs-locals'));
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'template'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Конфигурация Passport
+
 var passport = require('passport');
 var expressSession = require('express-session');
 app.use(expressSession({secret: 'mySecretKey'}));
@@ -37,7 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// Initialize Passport
+
 var initPassport = require('./passport/init');
 initPassport(passport);
 
@@ -45,22 +44,11 @@ var routes = require('./routes/index')(passport);
 app.use(flash());
 app.use('/', routes);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 
